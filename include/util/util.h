1174cf22074917958e3ffee63f71b71bc92cab25
ling <1730911462@qq.com>
2022-05-22 21:48:50 +0800

    :sparkles: feat: 新增获取getThreadID, createDir接口

--- include/util/util.h
+++ include/util/util.h
@@ -1,8 +1,8 @@
-/***********************************************************
+/**
  *@author ling
  *@date 2021/5/23
  *@brief 工具，一些函数,类
-***********************************************************/
+*/
 
 #ifndef WEBSERVER_UTIL_H
 #define WEBSERVER_UTIL_H
@@ -10,76 +10,80 @@
 #include <string>
 #include <cstdint>
 #include <cxxabi.h>
+#include <unistd.h>
 
-/**
- * @brief 禁止拷贝
- */
-class nonCopyAble
+namespace util
 {
-public:
-    nonCopyAble() = default;
-    ~nonCopyAble() = default;
+    /**
+     * @brief 设置信号处理方式为忽视
+     * @param sig 信号
+     */
+    void setSigIgn(int sig);
 
-private:
-    /// 禁用拷贝，也可以私有化拷贝构造函数和拷贝赋值运算符
-    nonCopyAble(const nonCopyAble&) = delete;
-    nonCopyAble& operator=(const nonCopyAble&) = delete;
-};
+    /**
+     * @brief 根据指定端口开启监听
+     * @param port 指定端口
+     * @return 成功返回listen_fd，失败返回-1
+     */
+    int socket_bind_listen(int port);
 
-const int LISTENQ = 1024;
+    /**
+     * @brief 设置指定fd为非阻塞模式
+     * @param fd 指定fd
+     * @return 成功返回0，失败返回-1
+     */
+    int setNonBlock(int fd);
 
-/**
- * @brief 设置信号处理方式为忽视
- * @param sig 信号
- */
-void setSigIgn(int sig);
+    /**
+     * @brief 多次调用read，读size个字节，直到对端关闭或者出现错误
+     */
+    int readn(int fd, char *buf, size_t size);
 
-/**
- * @brief 根据指定端口开启监听
- * @param port 指定端口
- * @return 成功返回listen_fd，失败返回-1
- */
-int socket_bind_listen(int port);
+    /**
+     * @brief 多次调用write，写size个字节，直到对端关闭或者出现错误。
+     */
+    int writen(int fd, const char *buf, size_t size);
 
-/**
- * @brief 设置指定fd为非阻塞模式
- * @param fd 指定fd
- * @return 成功返回0，失败返回-1
- */
-int setNonBlock(int fd);
+    /**
+     * @brief 根据文件后最得到文件类型名
+     * @param suffix 文件后缀形式
+     * @return 文件类型
+     */
+    std::string getMimeType(const std::string &suffix);
 
-/**
- * @brief 多次调用read，读size个字节，直到对端关闭或者出现错误
- */
-int readn(int fd, char* buf, size_t size);
+    /**
+     * @brief 获得当前时间(毫秒级)
+     */
+    std::uint64_t current_time();
 
-/**
- * @brief 多次调用write，写size个字节，直到对端关闭或者出现错误。
- */
-int writen(int fd, const char *buf, size_t size);
+    /**
+     * @brief 将类型名转化为字符串(typeinfo)
+     * @tparam T
+     * @return char* 类型名字符串
+     */
+    template<typename T>
+    const char *typeToName()
+    {
+        static const char *name = abi::__cxa_demangle(typeid(T).name(), nullptr,
+                                                      nullptr, nullptr);
+        return name;
+    }
 
-/**
- * @brief 根据文件后最得到文件类型名
- * @param suffix 文件后缀形式
- * @return 文件类型
- */
-std::string getMimeType(const std::string& suffix);
+    /**
+     * @brief   返回当前执行线程id
+     */
+    pid_t getThreadID();
 
-/**
- * @brief 获得当前时间(毫秒级)
- */
-std::uint64_t current_time();
+    /**
+     * @brief 从filePath中抽离, 目录路径出来
+     */
+    std::string getDir(const std::string& filePath);
 
-/**
- * @brief 将类型名转化为字符串(typeinfo)
- * @tparam T
- * @return char* 类型名字符串
- */
-template<typename T>
-const char* typeToName()
-{
-    static const char* name = abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, nullptr);
-    return name;
+    /**
+     * @brief 创建目录
+     * @param dirname 目录名
+     * @return 成功true, 失败false
+     */
+    bool createDir(const std::string& dirname);
 }
-
 #endif //WEBSERVER_UTIL_H
--- src/util/util.cpp
+++ src/util/util.cpp
@@ -1,159 +1,214 @@
 #include "util/util.h"
 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <csignal>
 #include <cstdio>
 #include <cstring>
 #include <unordered_map>
 
-void setSigIgn(int sig)
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <csignal>
+
+namespace util
 {
-    struct sigaction act;
-    act.sa_flags = 0;
-    act.sa_handler = SIG_IGN;
-    /// 信号处理函数执行时, 阻塞所有信号
-    sigfillset(&act.sa_mask);
-    if(-1 == sigaction(sig, &act, nullptr))
-        perror("sigaction");
-}
-
-int socket_bind_listen(int port)
-{
-    /// 检查port值，取正确区间范围
-    if(port < 1024 || port > 65535)
-        return -1;
-
-    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(listen_fd == -1)
-        return -1;
-    /// 消除bind时"Adress a'lready is used"的错误，
-    int optval = 1;
-    if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1)
-        return -1;
-
-    /// 设置服务器的IP和port，并且绑定到监听的socket描述符上。
-    struct sockaddr_in server_addr;
-    bzero(&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;// IP地址
-    server_addr.sin_port = htons(port);// 端口号
-    if(bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
-        return -1;
-
-    /// 开始监听
-    if(listen(listen_fd, LISTENQ) == -1)
-        return -1;
-    else
-        printf("服务器启动成功! 监听端口: %d\n", port);
-    return listen_fd;
-}
-
-int setNonBlock(int fd)
-{
-    int flag = 0;
-    int ret = fcntl(fd, F_GETFL, flag);
-    if(-1 == ret)
-        return -1;
-    flag |= O_NONBLOCK;
-    ret = fcntl(fd, F_SETFL, flag);
-    if(-1 == ret)
-        return -1;
-    return 0;
-}
-
-std::string getMimeType(const std::string& suffix)
-{
-    static std::unordered_map<std::string, std::string> ma;
-    /// 初始化一下ma, 之后就只能读ma。
-    if(ma.empty())
+    void setSigIgn(int sig)
     {
-        /// 这个不在标准中, 默认当作文本文件, 可以直接展示
-        ma.insert(std::pair<std::string, std::string>("default","text/plain"));
-        ma.insert(std::pair<std::string, std::string>(".html","text/html"));
-        ma.insert(std::pair<std::string, std::string>(".htm","text/html"));
-        ma.insert(std::pair<std::string, std::string>(".avi","video/x-msvideo"));
-        ma.insert(std::pair<std::string, std::string>(".bmp","timage/bmp"));
-        ma.insert(std::pair<std::string, std::string>(".c","text/plain"));
-        ma.insert(std::pair<std::string, std::string>(".doc","application/msword"));
-        ma.insert(std::pair<std::string, std::string>(".gif","image/gif"));
-        ma.insert(std::pair<std::string, std::string>(".gz","application/x-gzip"));
-        ma.insert(std::pair<std::string, std::string>(".ico","application/x-ico"));
-        ma.insert(std::pair<std::string, std::string>(".png", "image/png"));
-        ma.insert(std::pair<std::string, std::string>(".txt", "text/plain"));
-        ma.insert(std::pair<std::string, std::string>(".mp3", "audio/mp3"));
+        struct sigaction act;
+        act.sa_flags = 0;
+        act.sa_handler = SIG_IGN;
+        /// 信号处理函数执行时, 阻塞所有信号
+        sigfillset(&act.sa_mask);
+        if (-1 == sigaction(sig, &act, nullptr))
+            perror("sigaction");
     }
-    auto it = ma.find(suffix);
-    if(it != ma.end())
-        return it->second;
-    return ma["default"];
-}
 
-int readn(int fd, char* buf, size_t size)
-{
-    int total = 0;
-    int index = 0;
-    int num = 0;
-    while(size > 0)
+    int socket_bind_listen(int port)
     {
-        num = read(fd, (void*)(buf + index), size);
-        if(num > 0)
+        /// 检查port值，取正确区间范围
+        if (port < 1024 || port > 65535)
+            return -1;
+
+        int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (listen_fd == -1)
+            return -1;
+        /// 消除bind时"Adress a'lready is used"的错误，
+        int optval = 1;
+        if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1)
+            return -1;
+
+        /// 设置服务器的IP和port，并且绑定到监听的socket描述符上。
+        struct sockaddr_in server_addr;
+        bzero(&server_addr, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = INADDR_ANY;// IP地址
+        server_addr.sin_port = htons(port);// 端口号
+        if (bind(listen_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
+            return -1;
+
+        /// 开始监听
+        if (listen(listen_fd, 8) == -1)
+            return -1;
+        else
+            printf("服务器启动成功! 监听端口: %d\n", port);
+        return listen_fd;
+    }
+
+    int setNonBlock(int fd)
+    {
+        int flag = 0;
+        int ret = fcntl(fd, F_GETFL, flag);
+        if (-1 == ret)
+            return -1;
+        flag |= O_NONBLOCK;
+        ret = fcntl(fd, F_SETFL, flag);
+        if (-1 == ret)
+            return -1;
+        return 0;
+    }
+
+    std::string getMimeType(const std::string &suffix)
+    {
+        static std::unordered_map<std::string, std::string> ma;
+        /// 初始化一下ma, 之后就只能读ma。
+        if (ma.empty())
         {
-            total += num;
-            index += num;
-            size -= num;
+            /// 这个不在标准中, 默认当作文本文件, 可以直接展示
+            ma.insert(std::pair<std::string, std::string>("default", "text/plain"));
+            ma.insert(std::pair<std::string, std::string>(".html", "text/html"));
+            ma.insert(std::pair<std::string, std::string>(".htm", "text/html"));
+            ma.insert(std::pair<std::string, std::string>(".avi", "video/x-msvideo"));
+            ma.insert(std::pair<std::string, std::string>(".bmp", "timage/bmp"));
+            ma.insert(std::pair<std::string, std::string>(".c", "text/plain"));
+            ma.insert(std::pair<std::string, std::string>(".doc", "application/msword"));
+            ma.insert(std::pair<std::string, std::string>(".gif", "image/gif"));
+            ma.insert(std::pair<std::string, std::string>(".gz", "application/x-gzip"));
+            ma.insert(std::pair<std::string, std::string>(".ico", "application/x-ico"));
+            ma.insert(std::pair<std::string, std::string>(".png", "image/png"));
+            ma.insert(std::pair<std::string, std::string>(".txt", "text/plain"));
+            ma.insert(std::pair<std::string, std::string>(".mp3", "audio/mp3"));
         }
-        else if(num == 0)
+        auto it = ma.find(suffix);
+        if (it != ma.end())
+            return it->second;
+        return ma["default"];
+    }
+
+    int readn(int fd, char *buf, size_t size)
+    {
+        int total = 0;
+        int index = 0;
+        int num = 0;
+        while (size > 0)
         {
-            // 对端关闭才会读到0
-            return total;
+            num = read(fd, (void *) (buf + index), size);
+            if (num > 0)
+            {
+                total += num;
+                index += num;
+                size -= num;
+            }
+            else if (num == 0)
+            {
+                // 对端关闭才会读到0
+                return total;
+            }
+            else
+            {
+                if (errno == EAGAIN || errno == EWOULDBLOCK)
+                    break;// 对端数据读完了(正常情况), 退出
+                else if (errno == EINTR)
+                    continue;// 被信号中断, 回来继续
+                perror("read");
+                return -1;
+            }
+        }
+        return total;
+    }
+
+    int writen(int fd, const char *buf, size_t size)
+    {
+        int total = 0;
+        int num = 0;
+        while (size > 0)
+        {
+            num = write(fd, buf, size);
+            if (num > 0)
+            {
+                size -= num;
+                total += num;
+            }
+            else
+            {
+                if (errno == EINTR)
+                    continue;
+                perror("write");
+                return -1;
+            }
+        }
+        return total;
+    }
+
+    std::uint64_t current_time()
+    {
+        std::uint64_t ret = -1;
+        struct timeval tv;
+        if (-1 == gettimeofday(&tv, nullptr))
+            perror("gettimeofday");
+        else
+            ret = tv.tv_sec * (std::uint64_t) 1000 + tv.tv_usec / (std::uint64_t) 1000;
+        return ret;
+    }
+
+    pid_t getThreadID()
+    {
+        return (pid_t)syscall(SYS_gettid);
+    }
+
+    static int _mkdir(const char* dirname)
+    {
+        if(access(dirname, F_OK) == 0)
+            return 0;
+        return mkdir(dirname, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    }
+
+    bool createDir(const std::string& filePath)
+    {
+        /// mkdir函数只能在已存在的目录下创建目录, 如/xxx存在, 则创建/xxx/yyy是ok的,但是创建/xxx/yyy/zzz是不行的, 所以一层一层创建.
+        std::string buf;
+        for(char ch : filePath)
+        {
+            if(ch == '/')
+            {
+                if(-1 == _mkdir(buf.c_str()))
+                    return false;
+            }
+            buf.push_back(ch);
+        }
+        if(-1 == _mkdir(buf.c_str()))
+            return false;
+        return true;
+    }
+
+    std::string getDir(const std::string& filePath)
+    {
+        if(filePath.empty())
+            return ".";
+        auto pos = filePath.rfind('/');
+        if(pos == std::string::npos)
+        {// 没找到'/', 表明就是当前目录
+            return ".";
+        }
+        else if (pos == 0)
+        {// 说明路径是根目录下
+            return "/";
         }
         else
         {
-            if(errno == EAGAIN || errno == EWOULDBLOCK)
-                break;// 对端数据读完了(正常情况), 退出
-            else if(errno == EINTR)
-                continue;// 被信号中断, 回来继续
-            perror("read");
-            return -1;
+            return filePath.substr(0, pos + 1);
         }
     }
-    return total;
-}
-
-int writen(int fd, const char *buf, size_t size)
-{
-    int total = 0;
-    int num = 0;
-    while(size > 0){
-        num = write(fd, buf, size);
-        if(num > 0)
-        {
-            size -= num;
-            total += num;
-        }
-        else
-        {
-            if(errno == EINTR)
-                continue;
-            perror("write");
-            return -1;
-        }
-    }
-    return total;
-}
-
-std::uint64_t current_time()
-{
-    std::uint64_t ret = -1;
-    struct timeval tv;
-    if(-1 == gettimeofday(&tv, nullptr))
-        perror("gettimeofday");
-    else
-        ret = tv.tv_sec * (std::uint64_t)1000 + tv.tv_usec / (std::uint64_t)1000;
-    return ret;
 }
